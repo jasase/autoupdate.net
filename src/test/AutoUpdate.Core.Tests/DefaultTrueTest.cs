50b786b34448bda177c2733f07b5bb111cc400b2
Sascha Sternheim <sascha.sternheim@googlemail.com>
2018-11-26 19:54:16 +0100

    Added default green test for debug purposes

---
+++ src/test/AutoUpdate.Core.Tests/DefaultTrueTest.cs
@@ -0,0 +1,13 @@
+﻿using AutoUpdate.TestCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoUpdate.Core.Tests
+{
+    [TestClass]
+    public class DefaultTrueTest : Specification
+    {
+        [TestMethod]
+        public void Should_be_run_without_failure()
+        { }
+    }
+}
