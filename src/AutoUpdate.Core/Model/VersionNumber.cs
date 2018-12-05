52fc4e470f6f57c63d3cfa62b463dff6578227cb
Sascha Sternheim <sascha.sternheim@gmail.com>
2018-12-05 20:20:45 +0100

    Added compare interfaces and operators to VersionNumber Added unit test to test it

--- src/AutoUpdate.Core/Model/VersionNumber.cs
+++ src/AutoUpdate.Core/Model/VersionNumber.cs
@@ -1,6 +1,8 @@
-﻿namespace AutoUpdate.Core.Model
+﻿using System;
+
+namespace AutoUpdate.Core.Model
 {
-    public class VersionNumber
+    public class VersionNumber : IComparable<VersionNumber>, IEquatable<VersionNumber>
     {
         public int Major { get; set; }
         public int Minor { get; set; }
@@ -18,5 +20,27 @@
         public VersionNumber()
             : this(0, 0, 0, 0)
         { }
+
+        public int CompareTo(VersionNumber other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(VersionNumber other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator <(VersionNumber a, VersionNumber b)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public static bool operator >(VersionNumber a, VersionNumber b)
+        {
+            if (a == null) return false;
+            return b < a;
+        }
     }
 }
